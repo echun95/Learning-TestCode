b12fb8b21e89b30f2c59761bba22990879f72988
leeyoungchun <dldydcns123@gmail.com>
2022-06-01 23:31:02 +0900

    환경변수 값에따른 테스트 실행, os 별 테스트 실행, jre 버전 별 테스트 실행

--- src/test/java/com/study/testcode/TestcodeApplicationTests.java
+++ src/test/java/com/study/testcode/TestcodeApplicationTests.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Duration;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 
 class TestcodeApplicationTests {
@@ -58,7 +61,7 @@ class TestcodeApplicationTests {
 //			new Study(10);
 //			Thread.sleep(300);
 //		});
-		assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+		assertTimeoutPreemptively(Duration.ofMillis(400), () -> {
 			new Study(10);
 			Thread.sleep(300);
 		});
@@ -68,4 +71,52 @@ class TestcodeApplicationTests {
 		//때문에 트랜잭션의 롤백 처리를 받지 못하는 경우가 생길 수 있다. - 롤백과 상관없는 로직을 테스트할때만 사용하자.
 	}
 
+	@Test
+	void assumeTrueTest() {
+		String test_env = System.getenv("TEST_ENV");
+		//환경변수 값에 따라 테스트 실행 유무 결정하기
+		assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+		assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+			System.out.println("test_env = " + test_env);
+			new Study(-10);
+		});
+		assumingThat("DEV".equalsIgnoreCase(test_env), () -> {
+			System.out.println("test_env = " + test_env);
+			new Study(-10);
+		});
+
+		assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+	}
+	@Test
+	@EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+	void ENV_테스트1() throws Exception  {
+		System.out.println("TEST_ENV 환경 변수의 값이 LOCAL일때만 실행되는 테스트");
+	}
+	@Test
+	@EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "DEV")
+	void ENV_테스트2() throws Exception  {
+		System.out.println("TEST_ENV 환경 변수의 값이 DEV일때만 실행되는 테스트");
+	}
+
+	@Test
+	@EnabledOnOs({OS.MAC, OS.LINUX})
+	void OS_mac_linux_test() throws Exception  {
+		System.out.println("리눅스, 맥 os만 실행되는 테스트");
+	}
+	@Test
+	@EnabledOnOs({OS.WINDOWS})
+	void OS_window_test() throws Exception  {
+		System.out.println("window os만 실행되는 테스트");
+	}
+	@Test
+	@EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
+	void JRE_ver_test1() throws Exception  {
+		System.out.println("JRE 8,9,10,11 버전만 실행되는 테스트");
+	}
+	@Test
+	@EnabledOnJre(JRE.OTHER)
+	void JRE_ver_test2() throws Exception  {
+		System.out.println("JRE 옛날 버전만 실행되는 테스트");
+	}
 }
